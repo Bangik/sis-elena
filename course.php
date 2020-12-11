9529c10aa652d8237df8c3736a54bf024423b915
Hans701 <74651747+Hans701@users.noreply.github.com>
2020-12-11 17:20:36 +0700

    Update course.php
    Penambahan dropzone upload file

--- course.php
+++ course.php
@@ -1,62 +1,31 @@
-<?php
-
-  include 'templates/header.php';
-
-if ( isset($_POST{'submit'}) ){
-
-  $nama = $_FILES{'gambar'}{'name'};
-  $error = $_FILES{'gambar'}{'error'};
-  $size = $_FILES{'gambar'}{'size'};
-  $asal = $_FILES{'gambar'}{'tmp_name'};
-  $format = $_FILES{'gambar'}{'type'};
-
-  if ( $error == 0) {
-    if($size < 10000000){
-      if($format == 'image/jpeg'){
-        move_uploaded_file($asal, 'ipload/' . $nama);
-      }
-    }else{
-      echo '<script type="text/javascript">
-        alert("File terlalu besar/format salah");
-      </script>';
-    }
-  }else{
-    echo '<script type="text/javascript">
-      alert("error!");
-    </script>';
-  }
-
-}
-?>
-
-
-        <table width=30% border=1>
-          <tr bgcolor=#F5F5DC>
-            <td cospan=2>
-              <div class="col table-center">
-              <form action="course.php" method="post" ectype="multipart/form-data">
-                <a property="navbar-brand text-black"><h1>Status Tugas</h1></a>
-                <tbody>
-                  <tr>
-                    <td property="text" align="left">Status Pengumpulan
-                  </tr>
-                  <tr>
-                    <td property="text" align="left">Deadline
-                  </tr>
-                  <tr>
-                    <td property="text" align="left">Sisa Waktu
-                  </tr>
-                  <tr>
-                    <td property="text" align="left">File Tugas<input type="file" name="gambar">
-                    <input type="submit" name="submit" value="upload">
-                  </tr>
-                </tbody>
-              </div>
-            </td>
-          </tr>
-        </form>
-        </table>
-        <?php
-
-
-include 'templates/footer.php' ?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dropzone</title>
+	<link rel="stylesheet" type="text/css" href="asset/min/basic.min.css">
+	<link rel="stylesheet" href="asset/min/dropzone.min.css">
+	
+</head>
+<body>
+	<div class="container">
+		<form action="/file-upload" class="dropzone" id="my-awesome-dropzone"></form>
+	</div>
+	<style>
+		body {
+			background-color; #E5E5E5;
+		}
+		.container{
+			max-width; 757px;
+			margin: 400px auto 0;
+		}
+		.dropzone{
+			font-size; 18px;
+			font-family: monospace;
+			border-radius: 10px;
+			border: 2px dashed #E566B6F
+		}
+	</style>
+<script src="asset/min/dropzone.min.js">
+</script>
+</body>
+</html>
