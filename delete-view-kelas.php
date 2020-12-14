4607d3860e467ce64b5fe2d41e1cb2e419e951c5
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-14 21:07:33 +0700

    penambahan delete view kelas

--- delete-view-kelas.php
+++ delete-view-kelas.php
@@ -3,6 +3,10 @@
   global $link;
   if (isset($_POST['btnhapus'])) {
     $query = mysqli_query($link, "DELETE FROM materi_mapel WHERE id='$_POST[idform]'");
+    $query2 = mysqli_query($link, "DELETE FROM tugas WHERE kode_aktivitas_tugas='$_POST[idaktiv2]'");
+    $query3 = mysqli_query($link, "DELETE FROM tugas2 WHERE kode_aktivitas2='$_POST[idaktiv2]'");
+    $query4 = mysqli_query($link, "DELETE FROM presensi WHERE kode_aktivitas='$_POST[idaktiv1]'");
+    $query4 = mysqli_query($link, "DELETE FROM presensi2 WHERE kode_aktivitas='$_POST[idaktiv1]'");
     if ($query) {
       echo "<script>
               document.location='view-kelas.php?id=".$_POST['idkelasform']."';
