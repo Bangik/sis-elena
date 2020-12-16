f5810de57a0db8903d6280d9b9b0138a2fa8d2ea
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-16 18:37:26 +0700

    edit link

--- akademik-guru.php
+++ akademik-guru.php
@@ -37,7 +37,7 @@
         </div>
         <div class="col mb-4">
           <div class="card">
-            <a href="under-construction.html" class="text-color-a">
+            <a href="rekap-nilai.php" class="text-color-a">
               <img src="./asset/img/matematika-logo.jpg" class="card-img-top mx-auto d-block" alt="..." style="width:70%">
               <div class="card-body">
                 <h5 class="card-title">Rekap Nilai</h5>
