e890402d295dc2412d95f86a77d3009f0e9d35aa
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-18 11:01:38 +0700

    fix bug admin

--- templates/header-admin.php
+++ templates/header-admin.php
@@ -57,7 +57,7 @@
               <a class="nav-link fa fa-dashboard text-white" href="dashboard-admin.php">Dasboard <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link fa fa-mortar-board text-white" href="under-construction.html">Akademik</a>
+              <a class="nav-link fa fa-mortar-board text-white" href="akademik-admin.php">Akademik</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link fa fa-user-circle-o text-white dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
