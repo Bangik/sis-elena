5b35d9fde9d0b3e7fbb0e8176d142e92b4d553c1
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2020-12-26 21:50:07 +0700

    backup darurat

--- tes.php
+++ tes.php
@@ -1,72 +1,31 @@
 <?php
-  include 'templates/header.php';
-?>
-  <div class="container" >
-   <div class='content'>
-    <form action="upload.php" class="dropzone" id="dropzonewidget">
-      <input type="hidden" name="kode_siswa" value="1222">
-      <input type="hidden" name="kode_aktivitas" value="8">
-    </form>
+echo "<div class='d-flex justify-content-between'>
+  <a href='view-presensi.php?id= " . $tampilMateri['kode_aktivitas'] . "' class='card-text text-color-a' ><p> ". $tampilMateri['nama_presensi'] ."</p></a>
+  <form action='completes.php' method='post' class='float-right' id='forms'>
+    <input type='text' name='idform' value=' ". $tampilMateri['kode_aktivitas'] ."'>
+    <input type='text' name='nis' value='".$nis."'>
+    <input type='text' name='statusform' value='".$hasil['checkbox']."'>
+    <input type='text' name='kode_mapel_form' value='".$tampilMateri['kode_mapel']."'>
+    <button type='submit' name='btnchek_presensi' class= 'btn btn-link'>
+      <img src='./asset/img/".$hasil['checkbox'].".svg'>
+    </button>
+  </form>
+</div>"
 
-    <input type="button" id='uploadfiles' value='Simpan' >
-   </div>
-   <div class="col text-center">
-     <p class="btn btn-success" style="width: 10rem;" >Tambahkan Tugas</p>
-   </div>
-  </div>
 
-  <script type='text/javascript'>
-    Dropzone.autoDiscover = false;
-    $(".dropzone").dropzone({
-        maxFiles: 1,
-        autoProcessQueue: false,
-        init: function() {
-            myDropzone = this;
-            $.ajax({
-                url: 'upload.php',
-                type: 'post',
-                data: {request: 2},
-                dataType: 'json',
-                success: function(response){
-                  $.each(response, function(key,value) {
-                      var mockFile = { name: value.name, size: value.size };
+; ?>
 
-                      myDropzone.emit("addedfile", mockFile);
-                      myDropzone.emit("thumbnail", mockFile, value.path);
-                      myDropzone.emit("complete", mockFile);
-                  });
-                }
-            });
-        },
-        addRemoveLinks: true,
-         removedfile: function(file) {
-           var name = file.name;
 
-           $.ajax({
-             type: 'POST',
-             url: 'upload.php',
-             data: {name: name,request: 3},
-             sucess: function(data){
-                console.log('success: ' + data);
-             }
-           });
-           var _ref;
-            return (_ref = file.previewElement) != null ? _ref.parentNode.removeChild(file.previewElement) : void 0;
-         }
-    });
-    $('#uploadfiles').click(function(){
-      myDropzone.processQueue();
-      var data = $('#dropzonewidget').serialize()
 
-      $.ajax({
-        type: 'POST',
-        url: 'upload.php',
-        data: data
-      });
-    });
-
-  </script>
-
-<?php
-  include 'templates/footer.php'
-?>
+<div class="d-flex justify-content-between" >
+  <a href="view-presensi.php?id=<?php echo $tampilMateri['kode_aktivitas']; ?>" class="card-text text-color-a "><p><?php echo $tampilMateri['nama_presensi']; ?></p></a>
+  <form action="completes.php" method="post" class=" float-right " id="forms">
+    <input type="text" name="idform" value="<?php echo $tampilMateri['kode_aktivitas'];?>">
+    <input type="text" name="nis" value="<?php echo $nis?>">
+    <input type="text" name="statusform" value="<?php echo $hasil['checkbox'];?>">
+    <input type="text" name="kode_mapel_form" value="<?php echo $tampilMateri['kode_mapel'];?>">
+    <button type="submit" name="btnchek_presensi" class="btn btn-link" id="1">
+      <img src="./asset/img/<?php echo $hasil['checkbox']; ?>.svg">
+    </button>
+  </form>
+</div>
